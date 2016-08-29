daa2013707730f9a3add8bd0ea6a108c1fbab3a4
Kiran1552 <kiran.mahale@capgemini.com>
2016-08-29 22:32:00 +0530

    3rd exercise "WhenWorkingWithAPetMap" of Collection Kata is completed

--- src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithAPetMap.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/collections/exercises/WhenWorkingWithAPetMap.java
@@ -20,6 +20,7 @@ public class WhenWorkingWithAPetMap {
         pets.put("Fido", fido);
 
         // TODO
+        assertThat(pets.get("Fido"), equalTo(fido) );
     }
 
     @Test
@@ -33,6 +34,9 @@ public class WhenWorkingWithAPetMap {
         pets.put("Stray", stray);
 
         // TODO
+        Pet retrivedPet = pets.getOrDefault("Rover",stray);
+
+        assertThat(retrivedPet,equalTo(stray));
     }
 
     @Test
@@ -46,12 +50,13 @@ public class WhenWorkingWithAPetMap {
         pets.put("Felix", felix);
 
         // TODO
+        assertThat(pets.keySet(), containsInAnyOrder("Fido","Felix"));
     }
 
     @Test
     public void the_map_should_store_pets_in_alphabetical_order() {
         // TODO: Instantiate the correct type of Map
-        NavigableMap<String, Pet> pets = null;
+        NavigableMap<String, Pet> pets = new TreeMap<>();
 
         pets.put("Rover", Pet.dog().named("Rover"));
         pets.put("Felix", Pet.cat().named("Felix"));
@@ -63,7 +68,7 @@ public class WhenWorkingWithAPetMap {
     @Test
     public void the_map_should_store_pets_in_the_order_they_where_added() {
         // TODO: Instantiate the correct type of Map
-        Map<String, Pet> pets =  null;
+        Map<String, Pet> pets =  new LinkedHashMap<>();
 
         pets.put("Spot", Pet.cat().named("Spot"));
         pets.put("Rover", Pet.dog().named("Rover"));
@@ -76,7 +81,7 @@ public class WhenWorkingWithAPetMap {
     @Test
     public void the_map_should_store_pet_leaders_by_breed() {
         // TODO: Create an EnumMap to define a pet leader for each breed
-        EnumMap<Breed, Pet> petLeaders =  null;
+        EnumMap<Breed, Pet> petLeaders =  new EnumMap<Breed, Pet>(Breed.class);
 
         petLeaders.put(Breed.Cat, Pet.cat().named("Felix"));
         petLeaders.put(Breed.Dog, Pet.dog().named("Lassie"));

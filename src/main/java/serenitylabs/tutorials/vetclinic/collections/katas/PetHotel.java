a7b44b4121eb72e345601f45031c284c9f9fba4b
Kiran1552 <kiran.mahale@capgemini.com>
2016-08-30 00:27:24 +0530

    Adding "PetHotel.java" file.

---
+++ src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/PetHotel.java
@@ -0,0 +1,17 @@
+package serenitylabs.tutorials.vetclinic.collections.katas;
+
+import serenitylabs.tutorials.vetclinic.Pet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kiran on 8/30/2016.
+ */
+public class PetHotel {
+
+    public List<Pet> getPets() {
+
+        return new ArrayList<Pet>();
+    }
+}

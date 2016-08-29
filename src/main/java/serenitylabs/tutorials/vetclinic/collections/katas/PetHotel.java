dd1a78123af078ed0990ac7a6f516c6ade34851f
Kiran1552 <kiran.mahale@capgemini.com>
2016-08-30 00:48:05 +0530

    1st Test of Collection Kata "Step 1 - Check a pet into the hotel (part 1)" is completed

--- src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/PetHotel.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/collections/katas/PetHotel.java
@@ -2,16 +2,25 @@ package serenitylabs.tutorials.vetclinic.collections.katas;
 
 import serenitylabs.tutorials.vetclinic.Pet;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import static java.util.Comparator.comparing;
 
 /**
  * Created by Kiran on 8/30/2016.
  */
 public class PetHotel {
 
-    public List<Pet> getPets() {
+    private Collection<Pet> pets = new TreeSet<>(comparing(Pet::getName));
 
-        return new ArrayList<Pet>();
+    public Collection<Pet> getPets() {
+
+        return new ArrayList<>(pets);
+    }
+
+    public Collection<Pet> checkIn(Pet somePet) {
+        pets.add(somePet);
+        return pets;
+      //  return getPets();
     }
 }
